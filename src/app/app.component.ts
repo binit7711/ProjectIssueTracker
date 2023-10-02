fdb80092e6cc6d55c7ca21492dfac7b2b361b9c1
Binit <binit.rupakheti@cotiviti.com>
2023-10-02 08:23:26 +0545

    added minimal routes setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './components/auth/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, LoginComponent],
+  imports: [CommonModule, LoginComponent, RouterModule],
 })
 export class AppComponent {
   title = 'ProjectIssueTracker';
---
+++ src/app/app.route.ts
@@ -0,0 +1,16 @@
+import { Router, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, pathMatch: 'full' },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [],
+    pathMatch: 'full',
+  },
+];
