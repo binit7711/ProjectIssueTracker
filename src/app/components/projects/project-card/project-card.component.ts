8bec8e1160d4a62a3bf5626868b39dc23eb4ff7d
Binit <binit.rupakheti@cotiviti.com>
2023-10-04 15:50:53 +0545

    added project card

--- src/app/components/projects/project-card/project-card.component.ts
+++ src/app/components/projects/project-card/project-card.component.ts
@@ -1,25 +1,51 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Project } from '../project.model';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { DeleteDialogComponent } from '../delete-dialog/delete-dialog.component';
+import { ProjectsStore } from 'src/app/services/projects.store';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-card',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatGridListModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatDialogModule,
+  ],
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.scss'],
 })
 export class ProjectCardComponent {
+  readonly dialog = inject(MatDialog);
+  private readonly router = inject(Router);
+  private readonly projectStore = inject(ProjectsStore);
   @Input() project: Project = {
     id: '',
     name: '',
     description: '',
     ownerName: '',
   };
-  navigate(id: string) {
-    console.log(id);
+  navigate() {
+    this.router.navigate(['/home/your-projects', this.project.id]);
+  }
+  deleteProject(project: Project) {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      data: {
+        ...project,
+      },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result.data) {
+        this.projectStore.deleteProjectForUser(project.id);
+      }
+    });
   }
 }
