0e9dc49f91e37570d611e228231b8d6f80ae869a
Binit <binit.rupakheti@cotiviti.com>
2023-10-04 15:52:03 +0545

    dialog nullinjection error fix

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -4,6 +4,7 @@ import { ProjectsStore } from 'src/app/services/projects.store';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { Project } from './project.model';
 import { ProjectCardComponent } from './project-card/project-card.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-projects',
@@ -11,10 +12,16 @@ import { ProjectCardComponent } from './project-card/project-card.component';
   providers: [ProjectsStore],
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss'],
-  imports: [CommonModule, ProjectCardComponent, MatGridListModule],
+  imports: [
+    CommonModule,
+    ProjectCardComponent,
+    MatGridListModule,
+    MatDialogModule,
+  ],
 })
 export class ProjectsComponent implements OnInit {
   projectsStore = inject(ProjectsStore);
+
   projects = this.projectsStore.projects;
   ngOnInit(): void {
     this.projectsStore.fetchProjectsForUser();
