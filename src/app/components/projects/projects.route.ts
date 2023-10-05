a4a0118d296c87d7ef7986fc1424a2cc201be1cf
Binit <binit.rupakheti@cotiviti.com>
2023-10-05 17:54:21 +0545

    fix project route redirect

--- src/app/components/projects/projects.route.ts
+++ src/app/components/projects/projects.route.ts
@@ -3,6 +3,7 @@ import { ProjectsComponent } from './projects.component';
 import { ProjectItemComponent } from './project-item/project-item.component';
 
 export const routes: Route[] = [
+  { path: '', redirectTo: 'your-projects', pathMatch: 'full' },
   { path: 'your-projects', component: ProjectsComponent },
   { path: 'your-projects/:id', component: ProjectItemComponent },
 ];
